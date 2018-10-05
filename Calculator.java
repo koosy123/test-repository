29b574a75bfc3f66b54d36bc33a17dedae12f696
koosy123 <koosy123@naver.com>
2018-10-05 13:54:07 +0900

    minus 함수 추가

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,9 @@
 public class Calculator {
     public static int sum(int a, int b){
-        return a+b;
+        return a + b;
     }
-    
+    public static int minus(int a, int b){
+        return a - b;
+    }
+
 }
